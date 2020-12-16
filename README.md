dc7f5088aa7856f4e8bc47498ea185433743a564
Aristotle <11413392+tuxranger@users.noreply.github.com>
2020-12-16 15:25:08 -0500

    inital commit with dockerfiles, code, and readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# Application Purpose
+This application is to be used to tail logs for the logger service. 
+
+It can be used to get logs from the logger service, but it is intended to allow for tailing logs. 
+It will make the REST call to the service and tail those logs by repeatedly calling the given endpoint. 
+
+Logs retrieved will be delayed approximately 10 seconds.  
+# How to use
+## Docker container
+You can either get the Docker image from here: {insert link}
+
+Or build it using a command like this: 
+```
+docker build -f docker-tools/Dockerfile -t log-tailer:local .
+```
+
+Run the docker image with a command like this:
+
+```
+docker run --rm=true --network host --name log-tailer log-tailer:test <log tailer arguments>
+``` 
+
+## Arguments
+
+### Required
+
+* --url <url path>
+    * Pass the full url path for the logger service and the endpoint you wish to use
+    * i.e. http://localhost:8080/private/logger/pullLogs 
+
+### Optional
+* --jwt \<jwt>
+    * Pass the JWT to be used with the REST request. 
+* --follow
+    * Set this to tail the logs from the provided endpoint
+* --service \<service name>
+    * Pass the name of the service you want to follow when using the `pullLogsByService` endpoint
+* --transactionid \<transaction ID> 
+    * Pass the transaction ID of the transaction you want to follow when using the `pullLogsByTransaction` endpoint
+* --starttime \<timestamp or timeuuid>
+    * Pass the timestamp for the starting time to pull logs from that time forward 
+    * Timestamp must follow this layout: `yyyy-MM-dd'T'HH:mm:ss.SSS'Z'` i.e. 2006-01-02T15:04:05.000Z
+    * If a timeUUID is passed in the logs will return all logs after it. If timestamp, it will return logs matching that timestamp foward.
+* --endtime \<timestamp or timeuuid>
+    * Pass the timestamp for the ending time that logs should be pulled until.
+    * --follow will continue to pull logs after this timestamp. 
+    * Timestamp must follow this layout: `yyyy-MM-dd'T'HH:mm:ss.SSS'Z'` i.e. 2006-01-02T15:04:05.000Z
+* --showtimeuuid
+    * Set this to show timeUUID for each log. 
---
+++ main.go
@@ -0,0 +1,315 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var layout = "2006-01-02T15:04:05.000Z"
+var currentTimeUUID string
+var firstLogs = false
+var byTID = false
+var byService = false
+var jwt string
+var follow = false
+var url string
+var service string
+var transactionId string
+var startTime string
+var endTime string
+var showTimeUUID = false
+
+
+func main() {
+	args := os.Args[1:]
+	getArgs(args)
+
+	if url == "" {
+		log.Fatal("No url provided")
+	}
+
+	client := &http.Client{}
+
+	if byTID {
+		initGetLogs(url, transactionId, *client)
+	} else if byService {
+		initGetLogs(url, service, *client)
+	} else {
+		initGetLogs(url, "", *client)
+	}
+
+	if follow {
+		for {
+			endTime = time.Now().UTC().Format(layout)
+			time.Sleep(10*time.Second)
+			if byTID {
+				getLogs(url, transactionId, *client)
+			} else if byService {
+				getLogs(url, service, *client)
+			} else {
+				getLogs(url, "", *client)
+			}
+		}
+	}
+}
+
+func initGetLogs(url string, optional string, client http.Client) {
+	ts := timespan{
+		StartTime: time.Now().UTC().Add(-2*time.Minute).Format(layout),
+	}
+	if startTime != ""{
+		ts.StartTime = startTime
+	}
+	if endTime != "" {
+		ts.EndTime = endTime
+	}
+
+
+	if byTID {
+		ts.TransactionId = optional
+	}
+	if byService {
+		ts.Service = optional
+	}
+
+	bytesTS, _ := json.Marshal(ts)
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(bytesTS))
+	if err != nil {
+		log.Panicf("error while creating request: %v", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer " + jwt)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Panicf("error while performing request: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 401 {
+		fmt.Println("401 error")
+		fmt.Println("please provide a new jwt")
+		fmt.Scanln(&jwt)
+		return
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 200 {
+		fmt.Println("non 200 status")
+		log.Fatal(body)
+	}
+
+	dec := json.NewDecoder(bytes.NewReader(body))
+	// read open bracket
+	_, err = dec.Token()
+	if err != nil {
+		log.Println("failed looking for opening token")
+		log.Println(err)
+	}
+
+	// while the array contains values
+	for dec.More() {
+		var m logMsg
+		// decode an array value (Message)
+		err := dec.Decode(&m)
+		if err != nil {
+			log.Println("failed decoding")
+			log.Println(err)
+		}
+
+		fmt.Printf("%v: %v\n", m.TimeUUID, m.JsonLog.Log)
+		currentTimeUUID = m.TimeUUID
+	}
+
+	// read closing bracket
+	_, err = dec.Token()
+	if err != nil {
+		log.Println("failed looking for closing token")
+		log.Println(err)
+	}
+	if currentTimeUUID != "" {
+		firstLogs = true
+	}
+}
+
+func getLogs(url string, optional string, client http.Client) {
+	if firstLogs == false {
+		initGetLogs(url, optional, client)
+		return
+	}
+	ts := timespan{
+		StartTime: currentTimeUUID,
+		EndTime:   endTime,
+	}
+
+	if byTID {
+		ts.TransactionId = optional
+	}
+	if byService {
+		ts.Service = optional
+	}
+
+	bytesTS, _ := json.Marshal(ts)
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(bytesTS))
+	if err != nil {
+		log.Panicf("error while creating request: %v", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer " + jwt)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Panicf("error while performing request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 401 {
+		fmt.Println("401 error")
+		fmt.Println("please provide a new jwt")
+		fmt.Scanln(&jwt)
+		return
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 200 {
+		fmt.Println("non 200 status")
+		log.Fatal(body)
+	}
+	dec := json.NewDecoder(bytes.NewReader(body))
+	// read open bracket
+	_, err = dec.Token()
+	if err != nil {
+		log.Println("failed looking for opening token in loop")
+		log.Println(err)
+	}
+
+	// while the array contains values
+	for dec.More() {
+		var m logMsg
+		// decode an array value (Message)
+		err := dec.Decode(&m)
+		if err != nil {
+			log.Println("failed decoding in loop")
+			log.Println(err)
+		}
+
+		if showTimeUUID {
+			fmt.Printf("%v: %v\n", m.TimeUUID, m.JsonLog.Log)
+		} else {
+			fmt.Printf("%v\n", m.JsonLog.Log)
+		}
+		currentTimeUUID = m.TimeUUID
+	}
+
+	// read closing bracket
+	_, err = dec.Token()
+	if err != nil {
+		log.Println("failed looking for closing token in loop")
+		log.Println(err)
+	}
+}
+
+
+
+type timespan struct {
+	StartTime string `json:"startTime"`
+	EndTime string `json:"endTime"`
+	TransactionId string `json:"transactionId"`
+	Service string `json:"service"`
+}
+
+type logMsg struct {
+	TimeUUID string `json:"timeUUID"`
+	JsonLog logJson `json:"jsonLog"`
+}
+
+type logJson struct {
+	Container_id string `json:"container_id"`
+	Container_name string `json:"container_name"`
+	Source string `json:"source"`
+	Log string `json:"log"`
+	Ts_uuid string `json:"ts_uuid"`
+	Ts string `json:"ts"`
+	Year string `json:"year"`
+	Month string `json:"month"`
+	Day string `json:"day"`
+}
+
+func getArgs(args []string){
+	for _, v := range args {
+		if v == "--help" {
+			println("Use this app to retrieve and tail logs from the logger service")
+			println("required flags are: ")
+			println("--url <url path>: url for accessing the logger via REST. Include the endpoint you want to use")
+			println("")
+			println("optional flags are: ")
+			println("--follow: set this to tail logs")
+			println("--jwt <jwt>: the jwt for access the logger")
+			println("--service <service name>: the name of the service to get logs for the service endpoint")
+			println("--transactionid <transaction ID>: the transaction ID to get logs for the transaction endpoint")
+			println("--starttime <timestamp>: the timestamp for start time given in the form: " + layout)
+			println("--endtime <timestamp>: the timestamp for end time given in the form: " + layout)
+			println("--showtimeuuid: set this to show timeUUID for each log")
+		}
+		log.Fatal()
+	}
+
+	for k, v := range args {
+		if v == "--jwt" {
+			jwt = args[k+1]
+		}
+	}
+
+	for _, v := range args {
+		if v == "--follow" {
+			follow = true
+		}
+	}
+
+	for k, v := range args {
+		if v == "--url" {
+			url = args[k+1]
+		}
+	}
+
+	for k, v := range args {
+		if v == "--service" {
+			byService = true
+			service = args[k+1]
+		}
+	}
+
+	for k, v := range args {
+		if v == "--transactionid" {
+			byTID = true
+			transactionId = args[k+1]
+		}
+	}
+
+	for k, v := range args {
+		if v == "--starttime" {
+			startTime = args[k+1]
+		}
+	}
+
+	for k, v := range args {
+		if v == "--endtime" {
+			endTime = args[k+1]
+		}
+	}
+
+	for _, v := range args {
+		if v == "--showtimeuuid" {
+			showTimeUUID = true
+		}
+	}
+}
